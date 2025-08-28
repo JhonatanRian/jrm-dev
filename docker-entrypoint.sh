5ab07509266716524c0a2e218e36dcc7b8da1b65
JhonatanRian <jhonatanrian.jr@gmail.com>
2025-08-28 14:53:52 -0300

    fix: adjust

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,8 +7,18 @@
 echo "Rodando migrações projeto...";
 uv run manage.py migrate;
 
-echo "Rodando fixtures...";
-uv run manage.py loaddata users__00 stack__01 groupstack__02 project__03 sectionhero__04 portifolio__05;
+echo "Rodando fixtures users...";
+uv run manage.py loaddata users__00;
+echo "Rodando fixtures stack...";
+uv run manage.py loaddata stack__01;
+echo "Rodando fixtures groupstack...";
+uv run manage.py loaddata groupstack__02;
+echo "Rodando fixtures project...";
+uv run manage.py loaddata project__03;
+echo "Rodando fixtures sectionhero...";
+uv run manage.py loaddata sectionhero__04;
+echo "Rodando fixtures portifolio...";
+uv run manage.py loaddata portifolio__05;
 
 echo "Iniciando o servidor uwsgi";
 uv run uwsgi --ini ./conf/uwsgi.ini
