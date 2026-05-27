4db1726814e5a3c8fedc19407536f728cedb56c5
JhonatanRian <jhonatanrian.jr@gmail.com>
2026-05-27 18:53:39 -0300

    fix: remove automated execution of database fixtures from docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,8 +13,5 @@ uv run python manage.py compilemessages;
 echo "Rodando migrações projeto...";
 uv run manage.py migrate;
 
-echo "Rodando fixtures...";
-uv run manage.py loaddata users__00 portifolio;
-
 echo "Iniciando o servidor uwsgi";
 uv run uwsgi --ini ./conf/uwsgi.ini
